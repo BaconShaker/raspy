70222347938e741719c2a77ab9eccbe3fac2ada3
Spartikus <rshintani@gmail.com>
2015-07-25 15:22:06 -0500

    commented out

--- launcher.sh
+++ launcher.sh
@@ -9,24 +9,25 @@ make_env(){
 }
 
 vir_check=$(pip list | grep virtualenv)
-if [[  $vir_check  ]]; then
-	echo 'virtualenv has been installed!'
-else
-	echo "Need to install virtualenv"
-	pip install virtualenv
-	echo "Done"
-fi
-# Check if the environment root is in place, if not make it. 
-if [[ -d /envs ]]; then
-	echo "/envs already exists. "
-	if [[ -d /envs/laundry/bin ]]; then
-		echo "Virtual environment for laundry exists!"
-	else
-		echo "Virtual environment for laundry did not exist, let's make it."
-		make_env
-	fi
-else
-	cd /
-	mkdir envs
-	make_env
-fi
+echo $vir_check
+# if [[  $vir_check  ]]; then
+# 	echo 'virtualenv has been installed!'
+# else
+# 	echo "Need to install virtualenv"
+# 	pip install virtualenv
+# 	echo "Done"
+# fi
+# # Check if the environment root is in place, if not make it. 
+# if [[ -d /envs ]]; then
+# 	echo "/envs already exists. "
+# 	if [[ -d /envs/laundry/bin ]]; then
+# 		echo "Virtual environment for laundry exists!"
+# 	else
+# 		echo "Virtual environment for laundry did not exist, let's make it."
+# 		make_env
+# 	fi
+# else
+# 	cd /
+# 	mkdir envs
+# 	make_env
+# fi
